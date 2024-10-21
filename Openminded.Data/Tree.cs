4e31b8d05fbb76b626c85e555664f016c5f83e03
David Caspar <david.caspar@aol.ch>
2024-10-21 20:08:17 +0200

    Revert "Add Avalonia application and basic data structures."
    This reverts commit 424cf3ba1bdf6854a6d5115fadd694848c295639.

--- Openminded.Data/Tree.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Openminded.Data;
-
-public sealed record Tree<V>(V Value, List<Tree<V>> Children)
-{
-    public bool IsLeaf
-    {
-        get { return Children?.Count == 0; }
-    }
-}
